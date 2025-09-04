8371ce79c82e497d39961a3443679dffbc9e123f
Xiaoyu <xiaoyuyang48@gmail.com>
2025-09-04 23:52:21 +1000

    Add project files.

---
+++ LineUp/ConsoleGame.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace LineUp
+{
+    public static class ConsoleGame
+    {
+        public static void Start()
+        {
+            Console.WriteLine("Welcome to Line Up!");
+            // Game logic goes here
+        }
+    }
+}
---
+++ LineUp/GameEngine.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace LineUp
+{
+    public class GameEngine
+    {
+
+    }
+}
